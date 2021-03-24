a3449247a712cd6353099823d5b124d7bda8e785
Dania <dania.liu@ocsbstudent.ca>
2021-03-24 13:24:07 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// Sets input value to default.
+let age = 0
+
+// Connects the button to JavaScript, proceeds to function on click.
+document.getElementById('button').addEventListener('click', testAge)
+
+// How the function works.
+function testAge () {
+  age = document.getElementById('input').value
+  age = parseInt(age)
+  if (age >= 17) {
+    alert ('You can watch an R-rated movie alone.')
+  } else if (age >= 13) {
+    alert ('You can see a PG-13 movie alone.')
+  } else {
+    alert ('Hm. You are too young for most things.')
+  }
+}
